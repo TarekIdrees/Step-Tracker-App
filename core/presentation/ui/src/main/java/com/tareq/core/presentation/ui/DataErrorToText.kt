6a1be40373fb20f81c6705e25a7838c40eb0dcb0
TarekIdrees <tarekidrees5@gmail.com>
2024-07-15 13:27:08 +0200

    implement ui string helper functions

---
+++ core/presentation/ui/src/main/java/com/tareq/core/presentation/ui/DataErrorToText.kt
@@ -0,0 +1,32 @@
+package com.tareq.core.presentation.ui
+
+import com.tareq.core.domain.util.DataError
+
+fun DataError.asUiText(): UiText {
+    return when (this) {
+        DataError.Local.DISK_FULL -> UiText.StringResource(
+            R.string.error_disk_full
+        )
+        DataError.Network.REQUEST_TIMEOUT -> UiText.StringResource(
+            R.string.error_request_timeout
+        )
+        DataError.Network.TOO_MANY_REQUESTS -> UiText.StringResource(
+            R.string.error_too_many_requests
+        )
+        DataError.Network.NO_INTERNET_CONNECTION -> UiText.StringResource(
+            R.string.error_no_internet_connection
+        )
+        DataError.Network.PAYLOAD_TOO_LARGE -> UiText.StringResource(
+            R.string.error_payload_too_large
+        )
+        DataError.Network.SERVER_ERROR -> UiText.StringResource(
+            R.string.error_server_error
+        )
+        DataError.Network.SERIALIZATION -> UiText.StringResource(
+            R.string.error_serialization
+        )
+        else -> UiText.StringResource(
+            R.string.error_unknown
+        )
+    }
+}
